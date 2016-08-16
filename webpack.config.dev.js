3fdd7a45d52b2a12ab9607560cdfedaef13d110f
Jared Lunde <jared.lunde@gmail.com>
2016-08-16 17:42:14 -0500

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,75 @@
+var path = require('path')
+var webpack = require('webpack')
+var Dashboard = require('webpack-dashboard')
+var DashboardPlugin = require('webpack-dashboard/plugin')
+
+
+var dashboard = new Dashboard()
+
+
+module.exports = {
+  // The base directory for resolving the entry option
+  context: __dirname,
+  devtool: 'eval',
+
+  entry: {
+    app: 'index',
+  },
+
+  // Various output options, to give us a single bundle.js file with everything resolved and concatenated
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: path.join(__dirname, '/assets/'),
+    filename: "{{PKG_NAME}}.dev.js",
+    pathinfo: true
+  },
+
+  // Where to resolve our loaders
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    // Directories that contain our modules
+    modules: [path.resolve(__dirname, "lib"), "node_modules"],
+    descriptionFiles: ["package.json"],
+    moduleExtensions: ["-loader"],
+    // Extensions used to resolve modules
+    extensions: ['', '.js', '.scss', '.css']
+  },
+
+  module: {
+    loaders: [
+      /*
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style',
+          loader: 'css?group-css-media-queries!sass'
+        })
+      },
+      */
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [/node_modules/]
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({'process.env': {NODE_ENV: '"development"'}}),
+    /**
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor",
+                                             filename: "vendor.dev.js"}),
+    */
+    new DashboardPlugin(dashboard.setData),
+  ],
+
+  // Include mocks for when node.js specific modules may be required
+  node: {
+    fs: 'empty',
+    vm: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var path = require('path')
+var webpack = require('webpack')
+
+
+var stripLogger = 'strip-loader?strip[]=console.error' +
+                              '&strip[]=console.log' +
+                              '&strip[]=console.warn'
+
+
+module.exports = {
+  // The base directory for resolving the entry option
+  context: __dirname,
+
+  entry: {
+    app: 'index.js',
+  },
+
+  // Various output options, to give us a single bundle.js file with everything resolved and concatenated
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: "{{PKG_NAME}}.js",
+    pathinfo: true
+  },
+
+  // Where to resolve our loaders
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    // Directories that contain our modules
+    modules: [path.resolve(__dirname, "lib"), "node_modules"],
+    descriptionFiles: ["package.json"],
+    moduleExtensions: ["-loader"],
+    // Extensions used to resolve modules
+    extensions: ['', '.js', '.scss', '.css']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel', stripLogger],
+        exclude: [/node_modules/]
+      }
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
+    /**
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor",
+                                             filename: "vendor.js"}),
+    */
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+  ],
+
+  // Include mocks for when node.js specific modules may be required
+  node: {
+    fs: 'empty',
+    vm: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
